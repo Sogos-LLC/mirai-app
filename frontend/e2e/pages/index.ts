9bc931e1cd3350e664079ca4473050824cc2be25
John Yzaguirre <sojohnnysaid@gmail.com>
2026-02-06 03:06:35 -0500

    refactor

--- frontend/e2e/pages/index.ts
+++
@@ -1,6 +0,0 @@
-export { ContentLibraryPage } from './content-library.page';
-export { WizardPage } from './wizard.page';
-export { CourseEditorPage } from './course-editor.page';
-export { OutlinePage } from './outline.page';
-export { ExportPage } from './export.page';
-export { PreviewPage } from './preview.page';
